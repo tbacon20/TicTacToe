bd6386a7d4c1c982fe8a7549de3ccb6172954e94
wb385334 <96319225+wb385334@users.noreply.github.com>
2023-01-23 09:23:55 -0700

    Update Driver.cs

--- TicTacToe/Driver.cs
+++ TicTacToe/Driver.cs
@@ -6,7 +6,22 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+            char [] BoardArrayParameter = new char[9];
+
+            for (int i = 0, i < BoardArrayParameter.Length, i++)
+            {
+                BoardArrayParameter[i] = (char)i;
+            }
+
+
+            Console.WriteLine(BoardArrayParameter);
+            //new Board = bd(BoardArrayParameter);
+
+            //bd.displayBoard();
+
+            //Console.WriteLine('Player 1: Where would you like to go?');
         }
     }
 }
