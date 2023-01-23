3ce361dd938887a5e238641ea619a87f375955ac
wb385334 <96319225+wb385334@users.noreply.github.com>
2023-01-23 16:11:55 -0700

    Update Driver.cs

--- TicTacToe/Driver.cs
+++ TicTacToe/Driver.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace TicTacToe
 {
     class Driver
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
@@ -12,14 +14,85 @@ namespace TicTacToe
 
             for (int i = 1; i <= BoardArrayParameter.Length; i++)
             {
-                BoardArrayParameter[i] = (char)(i + '0');
+              
+               BoardArrayParameter[i] = (char)(i + '0');
+               
             }
 
-            //Board bd = new Board(BoardArrayParameter);
+            Console.WriteLine(BoardArrayParameter);
 
-            //bd.displayBoard();
+            new Board = bd(BoardArrayParameter);
 
-            //Console.WriteLine('Player 1: Where would you like to go?');
+            bool StillPlaying = true;
+            int turnCount = 0;
+            char PlayerMarker;
+            char PlayerNumber;
+            int PlayerTurn;
+            List <int> GuessList = new List <int>();
+
+            while (StillPlaying)
+            {
+                bd.displayBoard();
+
+                turnCount++;
+
+                if (turnCount % 2 ==1)
+
+                {
+                    PlayerNumber = '1';
+                    PlayerMarker = 'X';
+                }
+                else
+                {
+                    PlayerNumber = '2';
+                    PlayerMarker = 'O';
+                }
+
+               
+
+                Console.WriteLine("Player " + PlayerNumber + ": Where would you like to go?");
+                PlayerTurn = Convert.ToInt32(Console.ReadLine());
+
+
+
+                while (GuessList.Contains(PlayerTurn))
+                {
+                    Console.WriteLine("Please choose a vacant space to go.");
+                    PlayerTurn = Convert.ToInt32(Console.ReadLine());
+                }
+
+                GuessList.Add(PlayerTurn);
+
+                BoardArrayParameter[PlayerTurn - 1] = PlayerMarker;
+
+                bd.displayBoard(BoardArrayParameter);
+
+                string GameStatus = bd.determineWinner();
+
+                Console.WriteLine(GameStatus);
+
+                if (GameStatus == "Next Turn:")
+                {
+                    StillPlaying = true;
+                }
+                else
+                {
+                    StillPlaying = false;
+                }
+
+            }
+
+            Console.WriteLine("Good game! Play again? (Y/N)");
+            char PlayAgain = Convert.ToChar(Console.ReadLine());
+
+            if (PlayAgain == 'Y')
+            {
+                Main();
+            }
+
+
+
+
         }
     }
 }
