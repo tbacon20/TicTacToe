5697e0199d20fcec485cf70102ef30fd09d6a80b
Tanner Bacon <tannerbacon20@gmail.com>
2023-01-25 10:59:16 -0700

    Adding functions

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -26,6 +26,8 @@ namespace TicTacToe
 
         }
 
+
+        /*
         //Method to determine if there is a winner is based off of the current board from the last selection
         public string winnerDetermination(char[] currentBoard)
         {
@@ -141,6 +143,85 @@ namespace TicTacToe
             }
 
             //return the winning message
+            return winMsg;
+        }
+
+        */
+
+        //Method to determine if there is a winner is based off of the current board from the last selection
+        public string winnerDetermination(char[] currentBoard)
+        {
+            // variable to hold the message that will say if there was a winner of if its someone's turn
+            string winMsg;
+            bool win;
+            int i;
+
+            // Horizontal Winning Combinations
+            //increment the turn count
+            turnCount++;
+
+            //if the board is full call it a draw
+            if (turnCount == 9)
+            {
+                winMsg = "It's a Draw";
+                return winMsg;
+            }
+
+            // Check for Diagonal Wins
+            i = 0;
+            win = CheckWin(currentBoard, 0, 4);
+            if (!win)
+            {
+                win = CheckWin(currentBoard, 3, 2);
+            }
+            winMsg = WinMsg(currentBoard, i);
+
+            // Check for horizontal wins
+            i = 0;
+            while (!win && i < 9)
+            {
+                win = CheckWin(currentBoard, i, 1);
+                winMsg = WinMsg(currentBoard, i);
+                i = i + 3;
+            }
+
+            // Check for vertical wins
+            i = 0;
+            while (!win && i < 4)
+            {
+                win = CheckWin(currentBoard, i, 3);
+                winMsg = WinMsg(currentBoard, i);
+                i++;
+            }
+
+            //return the winning message
+            if (win) return winMsg;
+            else return "Next Turn";
+        }
+
+        private bool CheckWin(char[] board, int start, int iterator)
+        {
+            bool win = false;
+
+            if ((board[start] == board[start + iterator]) && (board[start + iterator] == board[start + (iterator * 2)]))
+                win = true;
+
+            return win;
+        }
+
+        private string WinMsg(char[] board, int winSpace)
+        {
+            string winMsg;
+
+            if (board[winSpace] == 'X')
+            {
+                winMsg = "Player 1 wins!";
+            }
+            else
+            {
+                winMsg = "Player 2 wins!";
+            }
+
             return winMsg;
         }
     }
